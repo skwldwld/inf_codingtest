7830a9ce84a636138be8779efdd222c2fbe6b58a
skwldwld <yezi0512@handong.ac.kr>
2026-04-29 16:10:36 +0900

    firstcommit

---
+++ week1/01_01_find_max_num.py
@@ -0,0 +1,9 @@
+def find_max_num(array):
+    # 이 부분을 채워보세요!
+
+    return max(array)
+
+
+print("정답 = 6 / 현재 풀이 값 = ", find_max_num([3, 5, 6, 1, 2, 4]))
+print("정답 = 6 / 현재 풀이 값 = ", find_max_num([6, 6, 6]))
+print("정답 = 1888 / 현재 풀이 값 = ", find_max_num([6, 9, 2, 7, 1888]))
