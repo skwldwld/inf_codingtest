ce61cb2e1998edc9d11c7c5c6df1c29a12f881af
skwldwld <yezi0512@handong.ac.kr>
2026-04-30 15:42:13 +0900

    w1_2

---
+++ week1/01_02_find_max_occurred_alphabet.py
@@ -0,0 +1,17 @@
+def find_max_occurred_alphabet(string):
+    data = dict()
+
+    for char in string:
+        if char == " ": continue
+        if char in data:
+            data[char] += 1
+        else:
+            data[char] = 1
+
+    return max(data, key=data.get)
+
+
+result = find_max_occurred_alphabet
+print("정답 = i 현재 풀이 값 =", result("hello my name is dingcodingco"))
+print("정답 = e 현재 풀이 값 =", result("we love algorithm"))
+print("정답 = b 현재 풀이 값 =", result("best of best youtube"))
