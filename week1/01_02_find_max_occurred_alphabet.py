d0411a3feb3b9247ee580066fad85106afaec13c
skwldwld <yezi0512@handong.ac.kr>
2026-05-06 21:42:51 +0900

    w1_2

--- week1/01_02_find_max_occurred_alphabet.py
+++ week1/01_02_find_max_occurred_alphabet.py
@@ -1,13 +1,12 @@
 def find_max_occurred_alphabet(string):
     data = dict()
+    alphabet_occurrence_array = [0] * 26
+
     for char in string:
         if char.isalpha():
-            if char in data:
-                data[char] += 1
-            else:
-                data[char] = 1
+            alphabet_occurrence_array[ord(char) - 97] += 1
 
-    return max(data, key=data.get)
+    return alphabet_occurrence_array[max(alphabet_occurrence_array) - 1]
 
 result = find_max_occurred_alphabet
 print("정답 = i 현재 풀이 값 =", result("hello my name is dingcodingco"))
