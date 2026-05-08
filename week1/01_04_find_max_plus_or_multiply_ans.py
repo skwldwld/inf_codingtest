93c302a73ec300bc1e7f2e7914743ef1c6a7d7f0
skwldwld <yezi0512@handong.ac.kr>
2026-05-08 20:42:50 +0900

    w1_4

---
+++ week1/01_04_find_max_plus_or_multiply_ans.py
@@ -0,0 +1,16 @@
+def find_max_plus_or_multiply(array):
+
+    result = 0
+    for i in array:
+        if i <= 1 or result <= 1:
+            result += i
+        else:
+            result *= i
+
+    return result
+
+
+result = find_max_plus_or_multiply
+print("정답 = 728 현재 풀이 값 =", result([0,3,5,6,1,2,4]))
+print("정답 = 8820 현재 풀이 값 =", result([3,2,1,5,9,7,4]))
+print("정답 = 270 현재 풀이 값 =", result([1,1,1,3,3,2,5]))
